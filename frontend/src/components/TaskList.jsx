54ec9633a77e087c8d7538ca2b4fbdf7acedcd45
AromalP2002 <akhilaromal938@gmail.com>
2025-01-08 09:34:54 +0530

    hlo

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -3,15 +3,23 @@ import axios from 'axios'
 
 function TaskList(){
     const [data,setData]=useState([])
+    const[editing,setEditing] = useState(false)
+    const[editDtls,setEditDtls] = useState({id:'',title:'',dis:''})
     useEffect(()=>{
         axios.get('http://127.0.0.1:8000/api/task/').then(res=>{
             console.log(res.data);
             setData(res.data)
         }).catch(error=>console.log(error.message))
     },[])
+    const editask = (task)=>{
+        setEditing(true)
+        setEditDtls(task)
+    }
     return(
         <>
-        <table>
+        <div className="container">
+
+        <table className="table">
             <thead>
                 <tr>
                     <th>Task</th>
@@ -23,12 +31,29 @@ function TaskList(){
                     <tr key={index}>
                         <td>{value.title}</td>
                         <td>{value.dis}</td>
-                        <td><button onClick={()=>{}} class="btn btn-secondary">Edit</button></td>
-                        <td><button onClick={()=>{}} class="btn btn-danger">Delete</button></td>
+                        <td><button onClick={()=>{editask(value)}} className="btn btn-secondary">Edit</button></td>
+                        <td><button onClick={()=>{}} className="btn btn-danger">Delete</button></td>
                     </tr>
                 ))}
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+            {editing ? <EditForm curTask={editDtls}/>:null}
+        </div>
+        </>
+    )
+}
+
+const EditForm=({curTask})=>{
+    console.log('EditForm',curTask);
+    const [task,setTask] = useState(curTask)
+
+    return(
+        <>
+            <form>
+                <input type="text" name="task" id="task" value={task.title}/>
+                <input type="text" name="dis" id="dis" value={task.dis} />
+                <input type="submit" value="update"  />
+            </form>
         </>
     )
 }
