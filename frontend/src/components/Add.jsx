1903a38b5409f589cae9a50d0fc8a6518c165646
AromalP2002 <akhilaromal938@gmail.com>
2025-01-10 09:07:11 +0530

    update

--- frontend/src/components/Add.jsx
+++ frontend/src/components/Add.jsx
@@ -1,11 +1,26 @@
-import axios from 'axios'
 import{ useState } from "react"
+import axios from 'axios'
+
 
 function Add(){
-    const[task,setTask] = useState('')
-    const[description,setDescription] = useState('')
+    const[title,settitle] = useState('')
+    const[dis,setDis] = useState('')
 
     const handlesubmit = (e)=>{
         e.preventDefault()
+        axios.post ('http://127.0.0.1:8000/api/task/',{title,dis}).then((res)=>{
+            settitle('')
+            setDis('')
+        }).catch(error=>console.log(error.message))
+        
     }
+
+    return(
+        <form onSubmit={handlesubmit} >
+            <input type="task" name="title" id="title" value={title} onChange={(e)=>settitle(e.target.value)}/>
+            <input type="text" name='dis' id="dis" value={dis} onChange={(e)=>setDis(e.target.value)}/>
+            <input type="submit" value="add"/>
+        </form>
+    )
 }
+export default Add    
